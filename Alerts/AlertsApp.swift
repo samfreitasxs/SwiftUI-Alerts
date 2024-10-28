b3ef071968e5319c109fc594325290f620c0b104
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-10-28 17:52:46 -0300

    first commit

---
+++ Alerts/AlertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsApp.swift
+//  Alerts
+//
+//  Created by Samuel Freitas on 28/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AlertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alerts/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Alerts
+//
+//  Created by Samuel Freitas on 28/10/24.
+//
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+    VStack (spacing: 40) {
+        Button("Fetch Data") {
+            // Network call Tails
+        }
+        
+        Button("Enter Data") {
+            
+        }
+        
+        Button("Log In") {
+            
+           }
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
