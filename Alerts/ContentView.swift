b3b63ba82938dfcbc426c6a4ca8c622914448f50
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-10-28 18:26:46 -0300

    Basic Alerts

--- Alerts/ContentView.swift
+++ Alerts/ContentView.swift
@@ -7,11 +7,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingBasicAlert = false
     
     var body: some View {
     VStack (spacing: 40) {
         Button("Fetch Data") {
             // Network call Tails
+            isShowingBasicAlert = true
+        }
+        .alert("No Network", isPresented: $isShowingBasicAlert) {
+            Button (" Try Again") {
+                //Do try again code
+            }
+            
+            Button ("Delete", role: .destructive){
+                //Do delete code
+            }
+            Button ("Cancel", role: .cancel) { }
+        } message: {
+            Text("No network detected. Connect to wi-fi or cellular and try again.")
         }
         
         Button("Enter Data") {
