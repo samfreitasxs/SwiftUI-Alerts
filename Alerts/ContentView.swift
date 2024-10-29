5d10e4aa628fce8e24eddef4292af7829cdfdbc1
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-10-29 08:47:48 -0300

    multiple buttons, text fields em alert, Error to an alert, Dynamic UI basead on Error

--- Alerts/ContentView.swift
+++ Alerts/ContentView.swift
@@ -8,6 +8,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isShowingBasicAlert = false
+    @State private var isShowingEnterValueAlert = false
+    @State private var isShowingLoginAlert = false
+    @State private var error: MyappError = .noNetwork
+    
+    @State private var valueString = ""
     
     var body: some View {
     VStack (spacing: 40) {
@@ -23,21 +28,78 @@ struct ContentView: View {
             Button ("Delete", role: .destructive){
                 //Do delete code
             }
-            Button ("Cancel", role: .cancel) { }
+            Button ("Cancel", role: .cancel) {  }
         } message: {
             Text("No network detected. Connect to wi-fi or cellular and try again.")
         }
         
         Button("Enter Data") {
+            isShowingEnterValueAlert = true
+        }
+        .alert("Enter Value", isPresented: $isShowingEnterValueAlert) {
+            TextField("Value",text: $valueString)
+            Button ("Submit") {
+                //Submit value
+            }
             
+            Button ("Cancel", role: .cancel){
+                
+            }
+        } message: {
+            Text( "Enter the dollar value of your item. ")
         }
         
         Button("Log In") {
-            
+            error = .invalidUserName
+            isShowingLoginAlert = true
            }
+        .alert(isPresented: $isShowingLoginAlert, error: error) { error in
+            if error == .invalidUserName {
+                TextField("Username", text: $valueString )
+                
+                Button("Submit") {
+                }
+                
+                Button ("Cancel", role: .cancel) {
+                    
+                }
+            }
+                
+        } message: { error in
+            Text ( error.failureReason)
+            }
         }
     }
 }
 #Preview {
     ContentView()
+}
+
+
+enum MyappError: LocalizedError {
+    case invalidUserName
+    case invalidPassword
+    case noNetwork
+    
+    var errorDescription: String? {
+        switch self {
+        case.invalidUserName:
+            "Invalid Username"
+        case.invalidPassword:
+            "Invalid Password"
+        case.noNetwork:
+            "No Network Connection "
+        }
+    }
+    
+    var failureReason: String {
+        switch self {
+        case.invalidUserName:
+            "The username entered does not exist in our database."
+        case.invalidPassword:
+            "The password entered for usernamen is incorrect."
+        case.noNetwork:
+            "Unable to detect a network connection. Please connect to wi-fi or cellular and try again."
+        }
+    }
 }
